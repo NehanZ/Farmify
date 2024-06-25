2d36fd5489886f67c3ed1bb8303e7b56ac72b7c5
Nehan Wijayagunarathna <nehannimsara0031029@gmail.com>
2024-06-25 23:18:54 +0530

    Add first
    Add main react files

---
+++ frontend/src/route/Routers.js
@@ -0,0 +1,25 @@
+import React from 'react' 
+import {Routes, Route, Navigate} from 'react-router-dom'
+
+import Home from '../pages/Home'
+import Crops from '../pages/Crops'
+import SearchResults from '../pages/SearchResults'
+import SignIn from '../pages/SignIn'
+import SignUp from '../pages/SignUp'
+import CropDetails from '../pages/CropDetails'
+
+const Routers = () => {
+  return (
+    <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/crops" element={<Crops />} />
+        <Route path="/crops/:id" element={<CropDetails />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/search" element={<SearchResults />} />
+    </Routes>
+  )
+}
+
+export default Routers
